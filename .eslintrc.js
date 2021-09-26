3f8c0a78dbc9bb47699b29114dd48a47c6c6b4da
derekaz <derekaz@users.noreply.github.com>
2021-09-26 16:40:08 -0700

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,9 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        // "prettier/@typescript-eslint",
+        "plugin:prettier/recommended"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
